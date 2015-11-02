9b36f742689dde0757de92090931ce5c3f4fca5a
JNL <jnlickey@users.noreply.github.com>
2015-11-02 10:44:17 -0500

    Update CodeSigningScript.ps1

--- CodeSigningScript/CodeSigningScript.ps1
+++ CodeSigningScript/CodeSigningScript.ps1
@@ -1,1 +1,7 @@
+# nlickey 20151012
+#
+# Code Signing Script
 
+$filepath = Read-Host -Prompt 'Enter complete path to file needing to be signed: '
+$cert=(dir cert:currentuser\my\ -CodeSigningCert)
+Set-AuthenticodeSignature $filepath $cert -TimestampServer http://timestamp.comodoca.com/authenticode
