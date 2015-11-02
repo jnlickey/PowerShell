9370ba3d82d444b332b30531fdc8fb9cea6d2315
JNL <jnlickey@users.noreply.github.com>
2015-11-02 10:46:48 -0500

    Delete CodeSigningScript.ps1

--- CodeSigningScript.ps1
+++
@@ -1,7 +0,0 @@
-# nlickey 20151012
-#
-# Code Signing Script
-
-$filepath = Read-Host -Prompt 'Enter complete path to file needing to be signed: '
-$cert=(dir cert:currentuser\my\ -CodeSigningCert)
-Set-AuthenticodeSignature $filepath $cert -TimestampServer http://timestamp.comodoca.com/authenticode
